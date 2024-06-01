6d1b8f667ecde1c7cff86329d53c32f589b5e811
Peter Jun Park <park@hey.com>
2024-06-01 11:24:37 -0400

    Update float highlights

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -8,19 +8,22 @@ return {
     variant = 'auto',
     styles = {
       bold = false,
-      italic = false
+      italic = false,
     },
     highlight_groups = {
+      -- mostly for Oil and Harpoon floats
+      NormalFloat = { fg = 'text', bg = 'overlay' },
+      FloatTitle = { fg = 'base', bg = 'pine' },
+      FloatBorder = { fg = 'overlay', bg = 'overlay' },
+
+      TelescopeTitle = { fg = 'base', bg = 'love' },
       TelescopeBorder = { fg = 'overlay', bg = 'overlay' },
-      TelescopeNormal = { fg = 'subtle', bg = 'overlay' },
+
       TelescopeSelection = { fg = 'text', bg = 'highlight_med' },
       TelescopeSelectionCaret = { fg = 'love', bg = 'highlight_med' },
       TelescopeMultiSelection = { fg = 'text', bg = 'highlight_high' },
-
-      TelescopeTitle = { fg = 'base', bg = 'love' },
       TelescopePromptTitle = { fg = 'base', bg = 'pine' },
       TelescopePreviewTitle = { fg = 'base', bg = 'iris' },
-
       TelescopePromptNormal = { fg = 'text', bg = 'surface' },
       TelescopePromptBorder = { fg = 'surface', bg = 'surface' },
     },
